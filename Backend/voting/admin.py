8b943ea909bca838f8f6c03e202aa8338b909116
Smarika Tripathi <smarikatripathi1@gmail.com>
2025-06-09 21:27:02 +0545

    managing

--- Backend/voting/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Voter, Candidate, Vote, Election
-
-# Register your models here.
-
-admin.site.register(Voter)
-admin.site.register(Candidate)
-admin.site.register(Vote)
-
